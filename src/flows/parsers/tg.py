ccfe8eba03337092b90c9e4f23c27afc3b36141a
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2023-12-30 21:02:04 +0000

    Revert "feat(parser): Rewrite tg parser to httpx"
    This reverts commit d491ed39ed0bc05639e8df8591a9b9e2307433ce.

--- src/flows/parsers/tg.py
+++ src/flows/parsers/tg.py
@@ -1,7 +1,7 @@
 from prefect import flow, get_run_logger
 from datetime import datetime
 
-from src.storage.parsers.tg import TelegramChannelScraper
+from src.storage.parsers import tg
 from src.storage.service import (
     get_telegram_sources_to_parse,
     insert_parsed_posts_from_telegram,
@@ -26,9 +26,8 @@ async def parse_telegram_sources(
     for tg_source in tg_sources:
         tg_username = tg_source["url"].split("/")[-1]  # is it ok?
 
-        tg = TelegramChannelScraper(tg_username)
-
-        posts = await tg.get_items(nposts)
+        # TODO: make scrapper async
+        posts = tg.parse_tg_channel(tg_username, num_of_posts=nposts)
         logger.info(f"Received {len(posts)} posts from @{tg_username}")
 
         await insert_parsed_posts_from_telegram(tg_source["id"], posts)
