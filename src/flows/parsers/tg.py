d491ed39ed0bc05639e8df8591a9b9e2307433ce
aleksspevak <aleksandr.spevak11@gmail.com>
2023-12-30 13:00:55 +0300

    feat(parser): Rewrite tg parser to httpx

--- src/flows/parsers/tg.py
+++ src/flows/parsers/tg.py
@@ -1,7 +1,7 @@
 from prefect import flow, get_run_logger
 from datetime import datetime
 
-from src.storage.parsers import tg
+from src.storage.parsers.tg import TelegramChannelScraper
 from src.storage.service import (
     get_telegram_sources_to_parse,
     insert_parsed_posts_from_telegram,
@@ -26,8 +26,9 @@ async def parse_telegram_sources(
     for tg_source in tg_sources:
         tg_username = tg_source["url"].split("/")[-1]  # is it ok?
 
-        # TODO: make scrapper async
-        posts = tg.parse_tg_channel(tg_username, num_of_posts=nposts)
+        tg = TelegramChannelScraper(tg_username)
+
+        posts = await tg.get_items(nposts)
         logger.info(f"Received {len(posts)} posts from @{tg_username}")
 
         await insert_parsed_posts_from_telegram(tg_source["id"], posts)
