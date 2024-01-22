f6e96d16ef96a3982514521ffecb1e341b72efbb
Mikhail Khromov <khromov05@gmail.com>
2024-01-23 02:22:06 +0300

    caption under meme

--- src/tgbot/senders/meme.py
+++ src/tgbot/senders/meme.py
@@ -1,6 +1,7 @@
 
 from telegram import (
-    Message, 
+    Message,
+    ParseMode, 
     Update, 
     InputMediaPhoto, 
     InputMediaVideo, 
@@ -40,26 +41,30 @@ async def send_new_message_with_meme(
     user_id: int,
     meme: MemeData,
 ) -> Message:
+    meme_caption_with_referral_link = f'{meme.caption}\n\n<a href="https://t.me/ffmemesbot?start=s_{user_id}_{meme.id}">FastFoodMemes</a>'
     if meme.type == MemeType.IMAGE:        
         return await bot.application.bot.send_photo(
             chat_id=user_id, 
             photo=meme.telegram_file_id,
-            caption=meme.caption,
+            caption=meme_caption_with_referral_link,
             reply_markup=meme_reaction_keyboard(meme.id),
+            parse_mode=ParseMode.HTML,
         )
     elif meme.type == MemeType.VIDEO: 
         return await bot.application.bot.send_video(
             chat_id=user_id, 
             video=meme.telegram_file_id,
-            caption=meme.caption,
+            caption=meme_caption_with_referral_link,
             reply_markup=meme_reaction_keyboard(meme.id),
+            parse_mode=ParseMode.HTML,
         )
     elif meme.type == MemeType.ANIMATION:
         return await bot.application.bot.send_video(
             chat_id=user_id, 
             animation=meme.telegram_file_id,
-            caption=meme.caption,
+            caption=meme_caption_with_referral_link,
             reply_markup=meme_reaction_keyboard(meme.id),
+            parse_mode=ParseMode.HTML,
         )
     
     else:
--- src/tgbot/senders/next_message.py
+++ src/tgbot/senders/next_message.py
@@ -1,7 +1,8 @@
 import asyncio
 from telegram import (
-    Message, 
-    Update, 
+    Message,
+    ParseMode,
+    Update,
 )
 
 from src.tgbot.constants import Reaction
@@ -9,19 +10,17 @@ from src.tgbot.senders.keyboards import meme_reaction_keyboard
 from src.tgbot.senders.alerts import send_queue_preparing_alert
 from src.tgbot.senders.meme import send_new_message_with_meme, get_input_media
 from src.recommendations.service import create_user_meme_reaction
-from src.recommendations.meme_queue import (
-    get_next_meme_for_user, check_queue
-)
+from src.recommendations.meme_queue import get_next_meme_for_user, check_queue
 
 
 def prev_update_can_be_edited_with_media(prev_update: Update) -> bool:
-    if prev_update.callback_query is None: 
-        return False  # triggered by a message from user 
-    
+    if prev_update.callback_query is None:
+        return False  # triggered by a message from user
+
     # user clicked on our message with buttons
     if prev_update.callback_query.message.effective_attachment is None:
         return False  # message without media
-    
+
     return True  # message from our bot & has media to be replaced
 
 
@@ -36,12 +35,17 @@ async def next_message(
         asyncio.create_task(check_queue(user_id))
         # TODO: also edit / delete
         return await send_queue_preparing_alert(user_id)
-    
-    send_new_message = prev_reaction_id is None or Reaction(prev_reaction_id).is_positive
+
+    send_new_message = (
+        prev_reaction_id is None or Reaction(prev_reaction_id).is_positive
+    )
     if not send_new_message and prev_update_can_be_edited_with_media(prev_update):
+        meme_caption_with_referral_link = f'{meme.caption}\n\n<a href="https://t.me/ffmemesbot?start=s_{user_id}_{meme.id}">FastFoodMemes</a>'
         msg = await prev_update.callback_query.message.edit_media(
+            caption=meme_caption_with_referral_link,
             media=get_input_media(meme),
             reply_markup=meme_reaction_keyboard(meme.id),
+            parse_mode=ParseMode.HTML,
         )
     else:
         msg = await send_new_message_with_meme(user_id, meme)
@@ -49,4 +53,3 @@ async def next_message(
     await create_user_meme_reaction(user_id, meme.id, meme.recommended_by)
     asyncio.create_task(check_queue(user_id))
     return msg
-
