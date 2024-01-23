9de3ce931f3a92583330e40018051a74ac3428cb
Mikhail Khromov <khromov05@gmail.com>
2024-01-23 22:29:05 +0300

    move util function to `meme.py`

--- src/tgbot/senders/meme.py
+++ src/tgbot/senders/meme.py
@@ -1,8 +1,7 @@
-
 from telegram import (
     Message,
-    InputMediaPhoto, 
-    InputMediaVideo, 
+    InputMediaPhoto,
+    InputMediaVideo,
     InputMediaAnimation,
 )
 from telegram.constants import ParseMode
@@ -16,7 +15,9 @@ from src.tgbot.senders.keyboards import meme_reaction_keyboard
 from src.recommendations.service import create_user_meme_reaction
 
 
-def get_input_media(meme: MemeData) -> InputMediaPhoto | InputMediaVideo | InputMediaAnimation:
+def get_input_media(
+    meme: MemeData,
+) -> InputMediaPhoto | InputMediaVideo | InputMediaAnimation:
     if meme.type == MemeType.IMAGE:
         return InputMediaPhoto(
             media=meme.telegram_file_id,
@@ -26,7 +27,7 @@ def get_input_media(meme: MemeData) -> InputMediaPhoto | InputMediaVideo | Input
         return InputMediaVideo(
             media=meme.telegram_file_id,
             caption=meme.caption,
-        ) 
+        )
     elif meme.type == MemeType.ANIMATION:
         return InputMediaAnimation(
             media=meme.telegram_file_id,
@@ -40,18 +41,22 @@ async def send_new_message_with_meme(
     user_id: int,
     meme: MemeData,
 ) -> Message:
-    meme_caption_with_referral_link = (f"{meme.caption}\n\n" if meme.caption else "") + f'<a href="https://t.me/ffmemesbot?start=s_{user_id}_{meme.id}">FastFoodMemes</a>'
-    if meme.type == MemeType.IMAGE:        
+    bot_username = bot.application.bot.username
+    meme_caption_with_referral_link = (
+        (f"{meme.caption}\n\n" if meme.caption else "")
+        + f'<a href="https://t.me/{bot_username}?start=s_{user_id}_{meme.id}">FastFoodMemes</a>'
+    )
+    if meme.type == MemeType.IMAGE:
         return await bot.application.bot.send_photo(
-            chat_id=user_id, 
+            chat_id=user_id,
             photo=meme.telegram_file_id,
             caption=meme_caption_with_referral_link,
             reply_markup=meme_reaction_keyboard(meme.id),
             parse_mode=ParseMode.HTML,
         )
-    elif meme.type == MemeType.VIDEO: 
+    elif meme.type == MemeType.VIDEO:
         return await bot.application.bot.send_video(
-            chat_id=user_id, 
+            chat_id=user_id,
             video=meme.telegram_file_id,
             caption=meme_caption_with_referral_link,
             reply_markup=meme_reaction_keyboard(meme.id),
@@ -59,12 +64,37 @@ async def send_new_message_with_meme(
         )
     elif meme.type == MemeType.ANIMATION:
         return await bot.application.bot.send_video(
-            chat_id=user_id, 
+            chat_id=user_id,
             animation=meme.telegram_file_id,
             caption=meme_caption_with_referral_link,
             reply_markup=meme_reaction_keyboard(meme.id),
             parse_mode=ParseMode.HTML,
         )
-    
+
     else:
         raise NotImplementedError(f"Can't send meme. Unknown meme type: {meme.type}")
+
+
+async def edit_last_message_with_meme(
+    user_id: int,
+    meme_id: int,
+    meme: MemeData,
+):
+    bot_username = bot.application.bot.username
+    meme_caption_with_referral_link = (
+        (f"{meme.caption}\n\n" if meme.caption else "")
+        + f'<a href="https://t.me/{bot_username}?start=s_{user_id}_{meme.id}">FastFoodMemes</a>'
+    )
+    await bot.application.bot.edit_message_media(
+        chat_id=user_id,
+        message_id=meme_id,
+        media=get_input_media(meme),
+        reply_markup=meme_reaction_keyboard(meme.id),
+    )
+    await bot.application.bot.edit_message_caption(
+        chat_id=user_id,
+        message_id=meme_id,
+        caption=meme_caption_with_referral_link,
+        parse_mode=ParseMode.HTML,
+        reply_markup=meme_reaction_keyboard(meme.id),
+    )
--- src/tgbot/senders/next_message.py
+++ src/tgbot/senders/next_message.py
@@ -3,12 +3,10 @@ from telegram import (
     Message,
     Update,
 )
-from telegram.constants import ParseMode
 
 from src.tgbot.constants import Reaction
-from src.tgbot.senders.keyboards import meme_reaction_keyboard
 from src.tgbot.senders.alerts import send_queue_preparing_alert
-from src.tgbot.senders.meme import send_new_message_with_meme, get_input_media
+from src.tgbot.senders.meme import edit_last_message_with_meme, send_new_message_with_meme
 from src.recommendations.service import create_user_meme_reaction
 from src.recommendations.meme_queue import get_next_meme_for_user, check_queue
 
@@ -40,16 +38,7 @@ async def next_message(
         prev_reaction_id is None or Reaction(prev_reaction_id).is_positive
     )
     if not send_new_message and prev_update_can_be_edited_with_media(prev_update):
-        msg = await prev_update.callback_query.message.edit_media(
-            media=get_input_media(meme),
-            reply_markup=meme_reaction_keyboard(meme.id),
-        )
-        meme_caption_with_referral_link = (f"{meme.caption}\n\n" if meme.caption else "") + f'<a href="https://t.me/ffmemesbot?start=s_{user_id}_{meme.id}">FastFoodMemes</a>'
-        await msg.edit_caption(
-            caption=meme_caption_with_referral_link,
-            parse_mode=ParseMode.HTML,
-            reply_markup=meme_reaction_keyboard(meme.id),
-        )
+        msg = await edit_last_message_with_meme(user_id, prev_update.callback_query.message.id, meme)
     else:
         msg = await send_new_message_with_meme(user_id, meme)
 
