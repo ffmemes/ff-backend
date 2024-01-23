6939f05824f14298de04a2ba969f0855d782522f
Mikhail Khromov <khromov05@gmail.com>
2024-01-23 21:59:29 +0300

    more fields to update in user_tg each time a user types /start

--- src/tgbot/service.py
+++ src/tgbot/service.py
@@ -11,7 +11,9 @@ from src.database import (
     user_tg,
     user_language,
     meme_raw_telegram,
-    execute, fetch_one, fetch_all,
+    execute,
+    fetch_one,
+    fetch_all,
 )
 
 from src.storage.constants import Language
@@ -21,13 +23,20 @@ async def save_tg_user(
     id: int,
     **kwargs,
 ) -> None:
+    update_dict_if_user_already_created = {
+        "username": kwargs["username"],
+        "first_name": kwargs["first_name"],
+        "last_name": kwargs["last_name"],
+        "is_premium": kwargs["is_premium"],
+        "language_code": kwargs["language_code"],
+        "updated_at": datetime.utcnow(),
+    }
     insert_statement = (
         insert(user_tg)
         .values({"id": id, **kwargs})
         .on_conflict_do_update(
             index_elements=(user_tg.c.id,),
-            set_={"updated_at": datetime.utcnow()},
-            # do we need to update more fields if a user already exists?
+            set_=update_dict_if_user_already_created,
         )
     )
 
