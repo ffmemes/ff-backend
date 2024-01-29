7d86fa4d47030ceb050c1a8e270b02236500502d
SUPERustam <498rustam@gmail.com>
2024-01-29 11:22:21 +0300

    Function deduplication with f-strings

--- src/storage/service.py
+++ src/storage/service.py
@@ -265,21 +265,22 @@ async def update_meme_status_of_ready_memes() -> list[dict[str, Any]]:
     return await fetch_all(update_query)
 
 
-async def find_meme_duplicate(**kwargs) -> int | None:
-    # For given meme finds a meme with the same content. 
-    # Returns the largest meme_id of the duplicates.
-    return None
-
-
-    # TODO:
-    select_query = f"""
-        SELECT 
-            M.id
-        FROM meme M
-        .........
-        ORDER BY M.id DESC
-        LIMIT 1
+async def find_meme_duplicate(imagetext: str) -> int | None:
     """
+    For given meme finds a meme with the same content. 
+    Returns the largest meme_id of the duplicates.
+    """
+
+    select_query = f"""
+       SELECT 
+           M.id 
+        FROM meme M
+        WHERE ocr_result IS NOT NULL 
+            AND similarity({imagetext}, M.ocr_result->>'text') >= 0.9
+        ORDER BY M.id DESC
+        LIMIT 1;
+    """
+
     res = await fetch_one(text(select_query))
     if res:
         return res["id"]
