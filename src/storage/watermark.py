7b42aacb28e3fd99413d936d897463058e577547
Anton <tosha_maz@mail.ru>
2024-02-02 17:52:15 +0300

    upd watermark

--- src/storage/watermark.py
+++ src/storage/watermark.py
@@ -43,42 +43,51 @@ def calculate_corners(img_w, img_h, text_bbox, margin) -> list:
     return corners
 
 def check_font_size(text, font_path, image, width_ratio):
-    breakpoint = width_ratio * image.size[0]
-    fontsize = 20
-    learning_rate = 5
-    font = ImageFont.truetype(font_path, fontsize)
-    while True:
-        if font.getlength(text) < breakpoint:
-            fontsize += learning_rate
-        else:
-            learning_rate = learning_rate // 2
-            fontsize -= learning_rate
-        font = ImageFont.truetype(font_path, fontsize)
-        if learning_rate <= 1:
-            break
-    return font
+    # breakpoint = width_ratio * image.size[0]
+    # fontsize = 20
+    # learning_rate = 5
+    fontsize = int(image.size[0] * width_ratio)
+    print(fontsize)
+    fontsize = ImageFont.truetype(font_path, fontsize)    
+    
+    # while True:
+    #     if font.getlength(text) < breakpoint:
+    #         fontsize += learning_rate
+    #     else:
+    #         learning_rate = learning_rate // 2
+    #         fontsize -= learning_rate
+    #     font = ImageFont.truetype(font_path, fontsize)
+    #     if learning_rate <= 1:
+    #         break
+    return fontsize
 
 def draw_corner_watermark(
     image_bytes: BytesIO,
     text: str,
     font_family: str = "Gidole-Regular.ttf",
+    font_path: str = "static",
     margin: int = 24
 ) -> Image:
+    
     with Image.open(image_bytes).convert("RGBA") as base:
         txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
         
         d = ImageDraw.Draw(txt)
-
-        width_ratio = .15  # Portion of the image the text width should be (between 0 and 1)
-        font_path = str(Path(Path.home(), "src", "fonts", font_family))
-        fnt = check_font_size(text, font_path, base, width_ratio)
-        # fnt = ImageFont.load_default()
+        # Portion of the image the text width should be (between 0 and 1)
+        width_ratio = .1
+        # font_path = str(Path(Path.home(), "static", "fonts", font_family))
+        # font_files_dir = Path(__file__).parent.parent
+        localization_files_dir = Path(__file__).parent.parent / "static/localization"
+        print(localization_files_dir, font)
+        with open(Path(localization_files_dir, "fonts", font_family), "r") as f:
+            font_path = f
+        fntsize = check_font_size(text, font_path, base, width_ratio)
+        font = ImageFont.truetype(font_path, fntsize)
         # calculate size of textbox
-        text_bbox = d.textbbox((0, 0), text, font=fnt)
+        text_bbox = d.textbbox((0, 0), text, font=font)
         # choose a random corner for the text
         corners = calculate_corners(img_w=base.size[0], img_h=base.size[1], text_bbox=text_bbox, margin=margin)
         text_position = random.choice(corners)
-        # text_position = choice(calculate_corners(img_w=base.size[0], img_h=base.size[1], text_bbox=text_bbox, margin=margin))
         # average brightness of pixel check and switch between black/white
         base_brightness = sum(base.getpixel(text_position)[:3]) / 3
         text_colour = select_wm_colour(base_brightness)
@@ -97,7 +106,7 @@ def add_watermark(image_content: bytes) -> BytesIO | None:
         image = draw_corner_watermark(
             image_bytes,
             text='@ffmemesbot',
-            text_size=18,
+            # text_size=18,
             margin=20
         )
     except Exception as e:
@@ -105,18 +114,18 @@ def add_watermark(image_content: bytes) -> BytesIO | None:
         return None
 
     buff = BytesIO()
-    buff.name = 'image_x.jpeg'
+    buff.name = 'image.jpeg'
     image.save(buff, 'JPEG')
     buff.seek(0)
 
-    return buff#image
+    return buff
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     
-#     # image_path = Path(Path.home(), "src", "test1.jpeg")  # Adjust the path if necessary
-#     image_path = Path(Path.home(), "src", "test1.jpeg")  # Adjust the path if necessary
-#     with open(image_path, 'rb') as image_file:
-#         image_bytes = image_file.read()
-#     watermarked_image = add_watermark(image_bytes)
-#     watermarked_image.save('/src/image_3x.jpg')
+    # image_path = Path(Path.home(), "src", "test1.jpeg")  # Adjust the path if necessary
+    image_path = Path(Path.home(), "src", "test2.jpeg")  # Adjust the path if necessary
+    with open(image_path, 'rb') as image_file:
+        image_bytes = image_file.read()
+    watermarked_image = add_watermark(image_bytes)
+    watermarked_image.save('/src/image_Xx.jpg')
    
