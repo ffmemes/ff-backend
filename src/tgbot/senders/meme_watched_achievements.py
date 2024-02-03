25317289d7660da1a066f22532434abb9a93cccf
Mikhail Khromov <khromov05@gmail.com>
2024-02-03 03:45:24 +0300

    more achievements

--- src/tgbot/senders/meme_watched_achievements.py
+++ src/tgbot/senders/meme_watched_achievements.py
@@ -9,15 +9,12 @@ from src.tgbot.bot import bot
 async def send_meme_watched_achievement_if_needed(user_id: int, user_info: dict) -> bool:
     """Send achievement about watching a certain amount of memes if needed and return True if sent"""
     memes_sent_count = user_info["nmemes_sent"]
-    text_localizer_name = None
-    if memes_sent_count == 100:
-        text_localizer_name = "achievement_100_meme_sent"
-    # elif memes_sent_count == 1000:
-    #     text_localizer_name = "achievement_1000_meme_sent"
+    meme_count_alerts = (100, 500, 1000, 5000, 10000, 50000, 100000)
 
-    if text_localizer_name is None:
+    if memes_sent_count not in meme_count_alerts:
         return False
 
+    text_localizer_name = f"achievement_{memes_sent_count}_meme_sent"
     await bot.send_message(
         chat_id=user_id,
         text=localizer.t(text_localizer_name, user_info["interface_lang"]),
