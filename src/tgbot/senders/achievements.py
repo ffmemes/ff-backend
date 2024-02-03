2023ca08981a83fdc61dd5c154c26907956c2c79
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-02-03 09:40:52 +0000

    fix moderator invite alert

--- src/tgbot/senders/achievements.py
+++ src/tgbot/senders/achievements.py
@@ -9,7 +9,7 @@ from src.tgbot.user_info import get_user_info
 
 async def send_achievement_if_needed(user_id: int) -> None:
     user_info = await get_user_info(user_id)
-    if user_info["type"] == UserType.USER and user_info["nmemes_sent"] > 1000:
+    if user_info["type"] == UserType.USER and user_info["nmemes_sent"] == 1000:
         await bot.send_message(
             chat_id=user_id,
             text="Do you wanna be a Moderator? 👀",
