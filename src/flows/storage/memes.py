25824b6489bcff27e648f6a2b805e6907b1df369
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-02-18 16:16:53 +0000

    limit number of vk memes to upload

--- src/flows/storage/memes.py
+++ src/flows/storage/memes.py
@@ -147,7 +147,7 @@ async def vk_meme_pipeline() -> None:
     await etl_memes_from_raw_vk_posts()
 
     logger.info("Getting unloaded memes to upload to Telegram.")
-    unloaded_memes = await get_unloaded_vk_memes()
+    unloaded_memes = await get_unloaded_vk_memes(limit=100)
     memes = await upload_memes_to_telegram(unloaded_memes)
 
     for meme in memes:
