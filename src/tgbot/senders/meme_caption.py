5c96bc70fc998f0fece50fc0c5e17f21a5df0d5b
Mikhail Khromov <44099594+VeryBigSad@users.noreply.github.com>
2024-02-22 00:31:54 +0300

    Fix HTML escaping in meme captions (#35)
    * Fix HTML escaping in meme captions
    
    * fix typo
    
    * use html.escape instead

--- src/tgbot/senders/meme_caption.py
+++ src/tgbot/senders/meme_caption.py
@@ -1,3 +1,5 @@
+from html import escape
+
 from src.storage.schemas import MemeData
 from src.tgbot.constants import UserType
 from src.tgbot.senders.utils import get_referral_html
@@ -7,7 +9,7 @@ from src.tgbot.user_info import get_user_info
 async def get_meme_caption_for_user_id(meme: MemeData, user_id: int) -> str:
     user_info = await get_user_info(user_id)
 
-    caption = meme.caption or ""
+    caption = escape(meme.caption, quote=False) if meme.caption else ""
 
     caption += "\n\n" + get_referral_html(user_id, meme.id)
 
