c5aaced108d9f0cf9e31fef82e543f35a0ff3a92
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-02-24 15:23:40 +0000

    more source langs

--- src/tgbot/senders/keyboards.py
+++ src/tgbot/senders/keyboards.py
@@ -51,6 +51,10 @@ def queue_empty_alert_keyboard(emoji: str = "⏳"):
 
 
 def meme_source_language_selection_keyboard(meme_source_id: int):
+    MEME_SOURCE_LANGUGES = ["ru", "uk", "en", "es", "de", "fa", "fr", "it"]
+    per_row = 4
+    # TODO:
+
     return InlineKeyboardMarkup(
         [
             [
@@ -60,7 +64,7 @@ def meme_source_language_selection_keyboard(meme_source_id: int):
                         meme_source_id=meme_source_id, lang_code=lang_code
                     ),
                 )
-                for lang_code in ["ru", "uk", "en", "es"]
+                for lang_code in ["ru", "uk", "en", "es", "fa"]
             ]
         ]
     )
