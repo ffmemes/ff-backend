e06a31acd5a35272172539113192f01fd7068508
Mikhail Khromov <khromov05@gmail.com>
2024-02-27 01:23:31 +0300

    fix connection_pool_size

--- src/tgbot/bot.py
+++ src/tgbot/bot.py
@@ -1,7 +1,9 @@
 # Used in cases when we need to send a message to a user
 
 from telegram import Bot
+from telegram.request import HTTPXRequest
 
 from src.config import settings
 
-bot = Bot(settings.TELEGRAM_BOT_TOKEN)
+request = HTTPXRequest(connection_pool_size=10)
+bot = Bot(settings.TELEGRAM_BOT_TOKEN, request=request)
