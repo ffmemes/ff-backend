09541a4138cc70dbc3bbb80ac4f39159e9cc05cd
Mikhail Khromov <khromov05@gmail.com>
2024-03-04 23:44:57 +0300

    move redis max connections to .env

--- src/config.py
+++ src/config.py
@@ -32,6 +32,8 @@ class Config(BaseSettings):
 
     MYSTIC_TOKEN: str | None = None
 
+    REDIS_MAX_CONNECTIONS: int | None = None
+
     # @model_validator(mode="after")
     # def validate_sentry_non_local(self) -> "Config":
     #     if self.ENVIRONMENT.is_deployed and not self.SENTRY_DSN:
