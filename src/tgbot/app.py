bb1a2c25e509ac05e27e9947ec481482eb22c0fe
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-05-07 21:26:42 +0100

    explain eng memes too

--- src/tgbot/app.py
+++ src/tgbot/app.py
@@ -22,8 +22,9 @@ from src.tgbot.constants import (
     MEME_SOURCE_SET_LANG_REGEXP,
     MEME_SOURCE_SET_STATUS_REGEXP,
     POPUP_BUTTON_CALLBACK_DATA_REGEXP,
-    # TELEGRAM_CHANNEL_EN_CHAT_ID,
+    TELEGRAM_CHANNEL_EN_CHAT_ID,
     TELEGRAM_CHANNEL_RU_CHAT_ID,
+    TELEGRAM_CHAT_EN_CHAT_ID,
     TELEGRAM_CHAT_RU_CHAT_ID,
     TELEGRAM_FEEDBACK_CHAT_ID,
 )
@@ -49,7 +50,7 @@ from src.tgbot.handlers.admin.waitlist import (
     handle_waitlist_invite_before,
 )
 from src.tgbot.handlers.chat.chat_member import handle_chat_member_update
-from src.tgbot.handlers.chat.explain_meme import explain_meme_ru
+from src.tgbot.handlers.chat.explain_meme import explain_meme_en, explain_meme_ru
 from src.tgbot.handlers.chat.feedback import (
     handle_feedback_message,
     handle_feedback_reply,
@@ -202,6 +203,15 @@ def add_handlers(application: Application) -> None:
             & filters.PHOTO
             & filters.SenderChat(TELEGRAM_CHANNEL_RU_CHAT_ID),
             callback=explain_meme_ru,
+        )
+    )
+
+    application.add_handler(
+        MessageHandler(
+            filters=filters.Chat(TELEGRAM_CHAT_EN_CHAT_ID)
+            & filters.PHOTO
+            & filters.SenderChat(TELEGRAM_CHANNEL_EN_CHAT_ID),
+            callback=explain_meme_en,
         )
     )
 
