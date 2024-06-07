5b53e414643d182fffa623b9326eaeb96c51ab4c
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-06-07 19:13:16 +0100

    Update uploaded_memes.py

--- src/flows/rewards/uploaded_memes.py
+++ src/flows/rewards/uploaded_memes.py
@@ -96,8 +96,9 @@ async def reward_ru_users_for_weekly_top_uploaded_memes():
             enternal_id=today,
         )
 
-        await log_meme_sent(top_meme["meme_id"], channel=Channel.TG_CHANNEL_RU)
-        await update_meme(top_meme["meme_id"], status=MemeStatus.PUBLISHED)
+        if top_meme["status"] != MemeStatus.PUBLISHED:
+            await update_meme(top_meme["meme_id"], status=MemeStatus.PUBLISHED)
+            await log_meme_sent(top_meme["meme_id"], channel=Channel.TG_CHANNEL_RU)
 
     # send message to tgchannelru
 
