0b7dbcb7905d5d41b9483569edf6ed55793c272a
Alex <alexander.ledovsky@gmail.com>
2024-09-05 23:39:55 +0300

    Added blender (#83)
    * added blender
    
    * added generate_with_blender
    
    * fixed await

---
+++ src/recommendations/blender.py
@@ -0,0 +1,95 @@
+import random
+from typing import Any
+
+EPS = 1e-6
+
+
+def blend(
+        candidates_dict: dict[str, list[dict[str, Any]]],
+        weights_dict: dict[str, float],
+        fixed_pos: dict[int, str] = None,
+        limit: int = 0,
+        random_seed: int = 42
+    ) -> list[dict[str, Any]]:
+    """
+    Blends candidates from multiple recommendation engines. Blending is implemented
+    as sampling with weights. Besides of that, it is possible to set fixed engines
+    to some positions.
+
+
+    Args:
+    - candidates_dict: Contains recommendation engine names with their outputs
+        Items in candidate lists must have "id" field
+    - weights_dict: Contains weights for each engine. Should have the same keys
+        as candidates_dict. Weights may not sum to 1
+    - fixed_pos: Allows to set fixed engines to provided positions. Starts from 0
+    - limit
+    - random_seed
+    """
+
+    random.seed(random_seed)
+
+    # input validation and processing
+    if set(candidates_dict.keys()) != set(weights_dict.keys()):
+        raise ValueError('Keys in candidates_dict and weights_dict do not match')
+
+    if fixed_pos:
+        for engine in fixed_pos.values():
+            if engine not in candidates_dict:
+                raise ValueError(f'Engine {engine} does not present in candidates_dict')
+
+    if limit == 0:
+        for candidates in candidates_dict.values():
+            limit += len(candidates)
+
+    # candidates_dict will be changed inplace further
+    candidates_dict = candidates_dict.copy()
+    for engine in candidates_dict.keys():
+        candidates_dict[engine] = candidates_dict[engine].copy()
+
+    # engines list is ensured to have non-empty engines
+    engines = [
+        engine for engine in candidates_dict.keys()
+        if len(candidates_dict[engine]) > 0
+    ]
+
+    weights = [ (weights_dict[engine] + EPS) for engine in engines ]
+    if len(engines) == 0:
+        return []
+
+    res = []
+
+    for res_idx in range(limit):
+        engine = None
+
+        # process fixed positions
+        if fixed_pos and res_idx in fixed_pos:
+            engine = fixed_pos[res_idx] if fixed_pos[res_idx] in engines else None
+
+        # sample engine
+        if engine is None:
+            engine = random.choices(population=engines, weights=weights)[0]
+
+        next_item = candidates_dict[engine][0].copy()
+        res.append(next_item)
+
+        # process candidates intersection
+        for engine in engines:
+            # remove all matches with next_item
+            stop = False
+            while not stop:
+                stop = True
+                for idx in range(len(candidates_dict[engine])):
+                    if next_item['id'] == candidates_dict[engine][idx]['id']:
+                        candidates_dict[engine].pop(idx)
+                        stop = False
+                        break
+
+        # maintain non-empty engines
+        engines = [engine for engine in engines if len(candidates_dict[engine]) > 0]
+        weights = [ (weights_dict[engine] + EPS) for engine in engines ]
+
+        if len(engines) == 0:
+            break
+
+    return res
---
+++ tests/recommendations/test_meme_queue.py
@@ -0,0 +1,248 @@
+from typing import Any
+
+import pytest
+
+from src.recommendations.candidates import CandidatesRetriever
+from src.recommendations.meme_queue import generate_with_blender
+
+
+@pytest.mark.asyncio
+async def test_generate_with_blender_below_30():
+    async def get_fast_dopamine(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 1},
+            {'id': 2},
+        ]
+
+    async def get_fast_dopamine_empty(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return []
+
+    async def get_best_memes_from_each_source(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 3},
+            {'id': 4},
+        ]
+
+    class TestRetriever(CandidatesRetriever):
+        engine_map = {
+            'fast_dopamine': get_fast_dopamine,
+            'best_meme_from_each_source': get_best_memes_from_each_source,
+        }
+    
+    candidates = await generate_with_blender(1, 10, 10, TestRetriever())
+    assert len(candidates) == 2
+    assert candidates[0]['id'] == 1
+    assert candidates[1]['id'] == 2
+
+    class TestRetriever(CandidatesRetriever):
+        engine_map = {
+            'fast_dopamine': get_fast_dopamine_empty,
+            'best_memes_from_each_source': get_best_memes_from_each_source,
+        }
+    
+    candidates = await generate_with_blender(1, 10, 10, TestRetriever())
+    assert len(candidates) == 2
+    assert candidates[0]['id'] == 3
+    assert candidates[1]['id'] == 4
+
+
+@pytest.mark.asyncio
+async def test_generate_with_blender_below_100():
+    async def uploaded_memes(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 1},
+            {'id': 2},
+        ]
+    async def get_fast_dopamine(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 3},
+            {'id': 4},
+        ]
+    async def get_best_memes_from_each_source(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 5},
+            {'id': 6},
+        ]
+    async def get_lr_smoothed(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 7},
+            {'id': 8},
+            {'id': 9},
+            {'id': 10},
+        ]
+
+
+    class TestRetriever(CandidatesRetriever):
+        engine_map = {
+            'uploaded_memes': uploaded_memes,
+            'fast_dopamine': get_fast_dopamine,
+            'best_memes_from_each_source': get_best_memes_from_each_source,
+            'lr_smoothed': get_lr_smoothed,
+        }
+    
+    candidates = await generate_with_blender(1, 10, 40, TestRetriever())
+    assert len(candidates) == 10
+    # hardcoded values
+    assert candidates[0]['id'] == 7
+    assert candidates[1]['id'] == 8
+    assert candidates[2]['id'] == 9
+    assert candidates[3]['id'] == 1
+    assert candidates[4]['id'] == 3
+    assert candidates[5]['id'] == 4
+
+@pytest.mark.asyncio
+async def test_generate_with_blender_above_100():
+    async def uploaded_memes(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 1},
+            {'id': 2},
+            {'id': 3},
+        ]
+    async def like_spread_and_recent_memes(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 4},
+            {'id': 5},
+            {'id': 6},
+        ]
+    async def get_lr_smoothed(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 7},
+            {'id': 8},
+            {'id': 9},
+            {'id': 10},
+        ]
+
+
+    class TestRetriever(CandidatesRetriever):
+        engine_map = {
+            'uploaded_memes': uploaded_memes,
+            'like_spread_and_recent_memes': like_spread_and_recent_memes,
+            'lr_smoothed': get_lr_smoothed,
+        }
+    
+    candidates = await generate_with_blender(1, 10, 200, TestRetriever(), random_seed=102)
+    assert len(candidates) == 10
+    # hardcoded values
+    assert candidates[0]['id'] == 7
+    assert candidates[1]['id'] == 8
+    assert candidates[2]['id'] == 1
+    assert candidates[3]['id'] == 9
+    assert candidates[4]['id'] == 2
+    assert candidates[5]['id'] == 10
+
+
+@pytest.mark.asyncio
+async def test_generate_with_blender_empty_above_100():
+    async def uploaded_memes(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return []
+
+    async def like_spread_and_recent_memes(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return []
+
+    async def get_lr_smoothed(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return []
+
+    async def top_memes_from_less_seen_sources(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 1},
+            {'id': 2},
+        ]
+
+    async def get_best_memes_from_each_source(
+        self,
+        user_id: int,
+        limit: int = 10,
+        exclude_meme_ids: list[int] = [],
+    ) -> list[dict[str, Any]]:
+        return [
+            {'id': 3},
+            {'id': 4},
+        ]
+
+
+    class TestRetriever(CandidatesRetriever):
+        engine_map = {
+            'uploaded_memes': uploaded_memes,
+            'like_spread_and_recent_memes': like_spread_and_recent_memes,
+            'lr_smoothed': get_lr_smoothed,
+            'less_seen_meme_and_source': top_memes_from_less_seen_sources,
+            'best_memes_from_each_source': get_best_memes_from_each_source,
+        }
+    
+    candidates = await generate_with_blender(1, 10, 200, TestRetriever())
+    assert len(candidates) == 2
+    assert candidates[0]['id'] == 3
+
+    candidates = await generate_with_blender(1, 10, 1200, TestRetriever())
+    assert len(candidates) == 2
+    assert candidates[0]['id'] == 1
