a1c30a5faddc71889272ed70ea47241c9d19f6f8
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2025-10-05 15:12:02 +0300

    Add admin command to promote moderators (#104)

--- src/tgbot/app.py
+++ src/tgbot/app.py
@@ -42,6 +42,7 @@ from src.tgbot.handlers.admin.broadcast_text import (
     handle_broadcast_text_ru_trigger,
 )
 from src.tgbot.handlers.admin.forward_channel import handle_forwarded_from_tgchannelru
+from src.tgbot.handlers.admin.promote_moderator import handle_promote_moderator
 from src.tgbot.handlers.admin.user_info import (
     DELETE_USER_DATA_CONFIRMATION_CALLBACK,
     delete_user_data,
@@ -249,6 +250,14 @@ def add_handlers(application: Application) -> None:
             filters=filters.ChatType.PRIVATE
             & filters.ForwardedFrom(chat_id=TELEGRAM_CHANNEL_RU_CHAT_ID),
             callback=handle_forwarded_from_tgchannelru,
+        )
+    )
+
+    application.add_handler(
+        CommandHandler(
+            "promotemod",
+            handle_promote_moderator,
+            filters=filters.ChatType.PRIVATE & filters.UpdateType.MESSAGE,
         )
     )
 
