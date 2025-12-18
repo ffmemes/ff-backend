6134406383e660531720c79a87b29816852b5f3e
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2025-12-19 01:51:12 +0400

    Update parsers.py

--- flow_deployments/parsers.py
+++ flow_deployments/parsers.py
@@ -1,36 +1,34 @@
+from pathlib import Path
 from prefect.client.schemas.schedules import CronSchedule
-from prefect.deployments import Deployment
 
 from src.config import settings
 from src.flows.parsers.ig import parse_ig_sources
 from src.flows.parsers.tg import parse_telegram_sources
 from src.flows.parsers.vk import parse_vk_sources
 
-deployment_tg = Deployment.build_from_flow(
-    flow=parse_telegram_sources,
-    name="Parse Telegram Sources",
+deployment_tg = parse_telegram_sources.from_source(
+    source=str(Path(__file__).parent.parent),
+    entrypoint="src/flows/parsers/tg.py:parse_telegram_sources",
+).deploy(
+    name="parse_telegram_sources",
     schedules=[CronSchedule(cron="40 * * * *", timezone="Europe/London")],
     work_pool_name=settings.ENVIRONMENT,
 )
 
-deployment_tg.apply()
-
-
-deployment_vk = Deployment.build_from_flow(
-    flow=parse_vk_sources,
-    name="Parse VK Sources",
-    work_pool_name=settings.ENVIRONMENT,
+deployment_vk = parse_vk_sources.from_source(
+    source=str(Path(__file__).parent.parent),
+    entrypoint="src/flows/parsers/vk.py:parse_vk_sources",
+).deploy(
+    name="parse_vk_sources",
     schedules=[CronSchedule(cron="20 * * * *", timezone="Europe/London")],
-)
-
-deployment_vk.apply()
-
-
-deployment_ig = Deployment.build_from_flow(
-    flow=parse_ig_sources,
-    name="Parse Instgram Sources",
     work_pool_name=settings.ENVIRONMENT,
-    schedules=[CronSchedule(cron="30 0 * * *", timezone="Europe/London")],
 )
 
-deployment_ig.apply()
+deployment_ig = parse_ig_sources.from_source(
+    source=str(Path(__file__).parent.parent),
+    entrypoint="src/flows/parsers/ig.py:parse_ig_sources",
+).deploy(
+    name="parse_ig_sources",
+    schedules=[CronSchedule(cron="30 0 * * *", timezone="Europe/London")],
+    work_pool_name=settings.ENVIRONMENT,
+)
